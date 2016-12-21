7011c65014535b320e57799365554be6af8faebb
Шпрыгин Ярослав <iarik2000.00@mail.ru>
2016-12-21 09:25:44 +0200

    Add files via upload

---
+++ ConsoleApplication10/ConsoleApplication10/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            int N, M, i, j;
+            int[ , ] n = new int [25, 25];
+            Random rnd = new Random();
+
+            Console.Write("Введите кол-во столбцов в массиве: ");
+            str = Console.ReadLine();
+            M = int.Parse(str);
+
+            Console.Write("Введите кол-во строк в массиве: ");
+            str = Console.ReadLine();
+            N = int.Parse(str);
+
+            for (i = 0; i < N; i++)
+            {
+                for (j = 0; j < M; j++)
+                {
+                    n[i,j] = rnd.Next(1,100);
+                    
+                    Console.Write("{0,4}", n[i,j]);
+                };
+                Console.WriteLine();
+            };
+            Console.WriteLine('\n');
+
+            Console.WriteLine("Новый массив" + '\n');
+            for (i = 0; i < N; i++)
+            {
+                for (j = 0; j < M; j++)
+                {
+                    n[i,j] = n[i,j + M/2];
+                    Console.Write("{0,4}", n[i, j]);
+                };
+                Console.WriteLine();
+            };
+                Console.ReadKey();
+        }
+    }
+}
