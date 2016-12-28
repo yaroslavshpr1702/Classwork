3363350ff643499f6353010f8a2bcd29bc94c2be
Шпрыгин Ярослав <iarik2000.00@mail.ru>
2016-12-28 09:20:47 +0200

    Add files via upload

---
+++ 28.12.2016/ConsoleApplication10/ConsoleApplication10/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите количество строк и столбцов: ");
+            string str = Console.ReadLine();
+            int K = int.Parse(str);
+
+            int[,] a = new int[K, K];
+            int sum = 0;
+            Random rnd = new Random();
+
+            for (int i = 0; i < K; i++)
+            {
+                for (int j = 0; j < K; j++)
+                {
+                    a[i, j] = rnd.Next(0, 9);
+                    if (i <= j)
+                        sum = sum + a[i, j];
+                    Console.Write("{0,4}", a[i, j]);
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Сумма элементов в заданной области: " + sum);
+
+            Console.ReadKey();
+        }
+    }
+}
